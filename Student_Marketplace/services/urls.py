2ed3d1969d86dc9d75224c7d39414eb537757ccf
Nima Makhmali <nmkhmly790@gmail.com>
2025-09-25 23:53:13 -0700

    feat(services): add simple list endpoints for categories and services

--- Student_Marketplace/services/urls.py
+++ Student_Marketplace/services/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import list_services
+from .views import list_services, list_categories, list_services_db
 
 urlpatterns = [
 	path('', list_services, name='services-list'),
+	path('categories/', list_categories, name='services-categories'),
+	path('list/', list_services_db, name='services-list-db'),
 ]
--- Student_Marketplace/services/views.py
+++ Student_Marketplace/services/views.py
@@ -1,5 +1,16 @@
 from django.http import JsonResponse
+from .models import Category, Service
 
 
 def list_services(_request):
     return JsonResponse({"services": []})
+
+
+def list_categories(_request):
+	categories = list(Category.objects.values("id", "name"))
+	return JsonResponse({"categories": categories})
+
+
+def list_services_db(_request):
+	services = list(Service.objects.values("id", "title", "price", "category_id"))
+	return JsonResponse({"services": services})
