5a2be2e4b30ba348ae06ab460bacabe38b7f2b8f
Nima Makhmali <nmkhmly790@gmail.com>
2025-09-25 23:52:50 -0700

    chore(services): register Category and Service in admin

--- Student_Marketplace/services/admin.py
+++ Student_Marketplace/services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Service
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ("id", "name")
+	search_fields = ("name",)
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+	list_display = ("id", "title", "price", "category")
+	list_filter = ("category",)
+	search_fields = ("title",)
